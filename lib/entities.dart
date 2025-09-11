3ea2d93a689085ac594d90a4768f8104b9d98da9
Ethan Lee <leeethan186@gmail.com>
2025-09-11 14:05:57 -0400

    Created database.dart

--- lib/entities.dart
+++ lib/entities.dart
@@ -1,5 +1,10 @@
-import 'package:flutter/material.dart';
 import 'dart:io';
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
+
+// part 'database.g.dart';
 
 // ------------------
 // ENTITY (TABLE)
