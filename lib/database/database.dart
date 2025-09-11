d61864c182857cb708d8ff3ee5d7d7f2374345d2
Ethan Lee <leeethan186@gmail.com>
2025-09-11 14:37:21 -0400

    Moved entities.dart to database folder.

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -5,7 +5,7 @@ import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as p;
-import 'package:hammer_ops/entities.dart';
+import 'entities.dart';
 
 part 'DAO.dart';
 part 'database.g.dart';
