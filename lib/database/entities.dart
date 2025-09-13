d1e294072a9ea1930f199bd3dd26c1db8f2e6d51
Ethan Lee <leeethan186@gmail.com>
2025-09-13 13:47:45 -0400

    Created Templates, TemplateFields, JobQuotes, QuoteFieldValues entities in database.

--- lib/database/entities.dart
+++ lib/database/entities.dart
@@ -10,10 +10,33 @@ class Users extends Table {
 }
 
 // ------------------
+// TEMPLATES (TABLE)
+// ------------------
+class Templates extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get name => text().withLength(min: 1, max: 100)();
+  IntColumn get createdBy => integer().customConstraint('REFERENCES users(id)')();
+  DateTimeColumn get createdAt => dateTime().withDefault(currentDateAndTime)();
+}
+
+// ------------------
+// TEMPLATE FIELDS (TABLE)
+// ------------------
+class TemplateFields extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  IntColumn get templateId => integer().customConstraint('REFERENCES templates(id) ON DELETE CASCADE')();
+  TextColumn get fieldName => text().withLength(min: 1, max: 100)();
+  TextColumn get fieldType => text().withLength(min: 1, max: 50)(); // e.g., text, number, date
+  BoolColumn get isRequired => boolean().withDefault(const Constant(false))();
+  IntColumn get sortOrder => integer().withDefault(const Constant(0))(); // For ordering fields within a template
+}
+
+// ------------------
 // QUOTES (TABLE)
 // ------------------
 class JobQuotes extends Table {
   IntColumn get id => integer().autoIncrement()();
+  IntColumn get templateId => integer().customConstraint('REFERENCES templates(id)')(); // each quote belongs to template and user
   TextColumn get customerName => text().withLength(min: 1, max: 100)();
   TextColumn get customerContact => text().withLength(min: 1, max: 100)();
   DateTimeColumn get quoteDate => dateTime().withDefault(currentDateAndTime)();
