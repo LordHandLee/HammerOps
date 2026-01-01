2159694074c07194305af177c238b3598cbfb7bb
Ethan Lee <ethanlee@MacBook-Air.local>
2026-01-01 11:14:55 -0500

    rebuilt web build

--- server/web_build/flutter_service_worker.js
+++ server/web_build/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "f9086029d2d229f5eef8eafc19202e1c",
+const RESOURCES = {"flutter_bootstrap.js": "0717e327bc2cfea9f0c4a2f4b00e5677",
 "version.json": "bf3d76783ff97b5978778fdbf1704fb1",
 "index.html": "aecd19617434253f3a8cd23eb9cc7f6f",
 "/": "aecd19617434253f3a8cd23eb9cc7f6f",
-"main.dart.js": "5ee8b60c37c0875127be4b5a9b636e01",
+"main.dart.js": "fe03f0b1d5579eed458fb12f76b50325",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -27,7 +27,7 @@ const RESOURCES = {"flutter_bootstrap.js": "f9086029d2d229f5eef8eafc19202e1c",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "33b7d9392238c04c131b6ce224e13711",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin": "185e8e02cc38a161a07aa07577e58077",
-"assets/fonts/MaterialIcons-Regular.otf": "30dd46b8ee3df72574bd311702e00cd7",
+"assets/fonts/MaterialIcons-Regular.otf": "9a0be2630c6a32d4f9f54f9cd9f5c3a2",
 "canvaskit/skwasm.js": "1ef3ea3a0fec4569e5d531da25f34095",
 "canvaskit/skwasm_heavy.js": "413f5b2b2d9345f37de148e2544f584f",
 "canvaskit/skwasm.js.symbols": "0088242d10d7e7d6d2649d1fe1bda7c1",
